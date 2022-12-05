e8b14a838b50d60ffebbbf8943934a5853a8e8dc
Nilanki Saha <114270905+nilanki-saha@users.noreply.github.com>
2022-12-06 01:35:21 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+a,b,c=float(input("1st no.:")),input("operator:"),float(input("2nd no:"))
+if b=='+':
+    print(a,"+",c,"=",a+c)
+if b=='-':
+    print(a,"-",c,"=",a-c)
+if b=='*':
+    print(a,"*",c,"=",a*c)
+if b=='%':
+    print(a,"%",c,"=",a%c)
+if b=='/':
+    print(a,"/",c,"=",a/c)
+if b=='**':
+    print(a,"**",c,"=",a**c)
+if b=='//':
+    print(a,"//",c,"=",a//c)
